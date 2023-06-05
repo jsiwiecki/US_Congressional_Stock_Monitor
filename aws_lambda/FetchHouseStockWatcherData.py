e48fd7cc11dca92f43c60e49ebba8f7c6c1bedc9
Jakub S <jsiwiecki@gmail.com>
2023-06-05 09:22:55 +0200

    lambda

--- aws_lambda/FetchHouseStockWatcherData.py
+++ aws_lambda/FetchHouseStockWatcherData.py
@@ -14,13 +14,10 @@ def save_to_s3(data, s3_bucket, s3_key):
     s3.put_object(Bucket=s3_bucket, Key=s3_key, Body=json.dumps(data))
 
 def lambda_handler(event, context):
-    # Set API URL (replace with the specific API endpoint you want to fetch)
     api_url = "https://housestockwatcher.com/api"
 
-    # Fetch data from API
     data = fetch_data(api_url)
 
-    # Save the fetched data to Amazon S3
     s3_bucket = os.environ["S3_BUCKET_NAME"]
     s3_key = "house_stock_watcher_data.json"
     save_to_s3(data, s3_bucket, s3_key)
