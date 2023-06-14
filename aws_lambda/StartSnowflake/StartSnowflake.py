cf3318d2fc8140451285ee353c308273f07ea095
Jakub S <jsiwiecki@gmail.com>
2023-06-14 16:05:37 +0200

    +lambda_startSnowflake

---
+++ aws_lambda/StartSnowflake/StartSnowflake.py
@@ -0,0 +1,32 @@
+import snowflake.connector
+import os
+
+
+SNOWFLAKE_ACCOUNT = os.getenv("SNOWFLAKE_ACCOUNT")
+SNOWFLAKE_USER = os.getenv("SNOWFLAKE_USER")
+SNOWFLAKE_PASSWORD = os.getenv("SNOWFLAKE_PASSWORD")
+SNOWFLAKE_WAREHOUSE = os.getenv("SNOWFLAKE_WAREHOUSE")
+SNOWFLAKE_DATABASE = os.getenv("SNOWFLAKE_DATABASE")
+SNOWFLAKE_SCHEMA = os.getenv("SNOWFLAKE_SCHEMA")
+ROLE_NAME = os.getenv("SNOWFLAKE_ROLE")
+
+
+ctx = snowflake.connector.connect(
+    account=SNOWFLAKE_ACCOUNT,
+    user=SNOWFLAKE_USER,
+    password=SNOWFLAKE_PASSWORD,
+    warehouse=SNOWFLAKE_WAREHOUSE,
+    database=SNOWFLAKE_DATABASE,
+    schema=SNOWFLAKE_SCHEMA
+)
+
+cur = ctx.cursor()
+
+try:
+    cur.callproc("orchestrate_process")
+    print("Stored procedure executed successfully.")
+except Exception as e:
+    print("An error occurred:", e)
+finally:
+    cur.close()
+    ctx.close()
